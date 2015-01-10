5897db6cee7aacd46a67f77ca0aa5ed9c597e571
Alvin <alvin990228@gmail.com>
2015-01-10 23:35:06 +0800

    2015.01.10 Alvin+ : IRQ works now but have bugs!!

--- f.c
+++ f.c
@@ -5,34 +5,8 @@ void f(){
 
 	while(1){
 
-			led_allon();
-
-			led_delay(10);
-
-			led_noff(2);
-
-			led_delay(10);
-
-			led_noff(3);
-		
-			led_delay(10);
-
-			led_noff(1);
-			
-			led_delay(10);
-
-			led_noff(4);
-			
-			led_delay(15);
-
-			led_flash(3);
-			led_delay(20);
-
-			led_shr(2);
-
-			led_delay(13);
-
-			led_shl(4);		
+		led_shr(1);
+		led_shl(1);		
 	}
 
         
